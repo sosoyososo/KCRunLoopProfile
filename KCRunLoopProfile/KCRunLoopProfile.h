db1c20ecd927ec111df8a35297833edac7d6f017
karsa <wanghongtao114@gmail.com>
2018-07-02 17:08:29 +0800

    Initial Commit

---
+++ KCRunLoopProfile/KCRunLoopProfile.h
@@ -0,0 +1,19 @@
+//
+//  KCRunLoopProfile.h
+//  KCRunLoopProfile
+//
+//  Created by karsa on 2018/7/2.
+//  Copyright © 2018年 karsa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KCRunLoopProfile.
+FOUNDATION_EXPORT double KCRunLoopProfileVersionNumber;
+
+//! Project version string for KCRunLoopProfile.
+FOUNDATION_EXPORT const unsigned char KCRunLoopProfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KCRunLoopProfile/PublicHeader.h>
+
+
